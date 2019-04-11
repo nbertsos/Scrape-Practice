4e57b3c68eea5427c60c8205dba1246c621e15fe
nbertsos <48657899+nbertsos@users.noreply.github.com>
2019-04-11 12:57:53 -0400

    Add files via upload

---
+++ 12_51.py
@@ -0,0 +1,22 @@
+import re
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urllib(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+tags = soup('span')
+
+total = 0
+
+for span in tags :
+    value = int(span)
+    total += value
+
+print(total)
